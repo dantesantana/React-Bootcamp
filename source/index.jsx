a2f49bc64a796035aa4553d704fe5f76c4eacd33
Leon Pearce <leonp1991@gmail.com>
2018-03-22 15:13:26 +0800

    REVERT: source code to masters content

--- source/index.jsx
+++ source/index.jsx
@@ -1,50 +1,13 @@
-import React       from 'react';
-import ReactDOM    from 'react-dom';
-import GetArticles from 'hacker-news-top-list';
-
-import Filter      from './Views/Filter';
-import NewsFeed    from './Views/NewsFeed';
-
-import styles from './styles.css';
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      request: 'LOADING',
-      filter: '',
-    };
-  }
-
-  componentWillMount() {
-    GetArticles().then((articles) => {
-      this.setState({ articles, request: 'SUCCESS' });
-    }).catch(() => {
-      this.setState({ request: 'FAILURE' });
-    });
-  }
-
-  handleFilterChange(event) {
-    this.setState({ filter: event.target.value })
-  }
-
-  render() {
-    return (
-      <div className={styles.app}>
-        <Filter
-          onChange={this.handleFilterChange}
-          value={this.state.filter}
-        />
-        <NewsFeed
-          articles={this.state.articles}
-          request={this.state.request}
-        />
-      </div>
-    );
-  }
+function Main() {
+  document.body.innerHTML = `
+    <div>
+      <span>
+        Hello Hacker News
+      </span>
+    </div>
+  `
 }
 
-
-ReactDOM.render(<App />, document.getElementById('App'));
+Main()

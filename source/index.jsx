ff565bd5ecee221b733894a8b24d381c0a95180d
Leon Pearce <leonp1991@gmail.com>
2018-03-22 11:27:42 +0800

    ADD: source code for chapter-two content

--- source/index.jsx
+++ source/index.jsx
@@ -1,10 +1,21 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
 
+export default function Article(props) {
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <p>by {props.by}</p>
+      <a href={props.url}>{props.url}</a>
+    </div>
+  )
+}
+
+
 ReactDOM.render((
-  <div>
-    <span>
-      Hello Hacker News
-    </span>
-  </div>
+  <Article
+    by="Hoolio"
+    title="Some Cool Techy Stuff"
+    url="https://google.com"
+  />
 ), document.getElementById('App'));

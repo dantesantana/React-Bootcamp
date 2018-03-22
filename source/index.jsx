48c866f70a11840f16ccf8fd55ae4d2355759c53
Leon Pearce <leonp1991@gmail.com>
2018-03-22 10:53:25 +0800

    ADD: source code for chapter-one content

--- source/index.jsx
+++ source/index.jsx
@@ -1,8 +1,10 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(React.createElement(
-  'div',
-  {},
-  'Hello Hacker News'
+ReactDOM.render((
+  <div>
+    <span>
+      Hello Hacker News
+    </span>
+  </div>
 ), document.getElementById('App'));

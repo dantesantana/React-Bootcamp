99ab054d3c7a177d747b685b3a5484b81b4b718d
Leon Pearce <leonp1991@gmail.com>
2018-03-23 12:28:28 +0800

    ADD: tanda logo :beer:

--- .github-pages/src/components/Header/index.js
+++ .github-pages/src/components/Header/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import logo from './logo.png'
+
+
 const Header = () => (
   <div
     style={{
@@ -15,18 +18,20 @@ const Header = () => (
         padding: '3rem 1rem',
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            fontWeight: '300',
-            textDecoration: 'none',
-          }}
-        >
-          Tanda React Bootcamp
-        </Link>
-      </h1>
+      <Link
+        to="/"
+        style={{
+          alignItems: 'center',
+          color: 'white',
+          display: 'flex',
+          textDecoration: 'none',
+        }}
+      >
+        <img style={{ padding: '0 1rem 0.125rem 0', margin: '0' }} src={logo} />
+        <h2 style={{ fontWeight: '300', margin: '0' }}>
+            React Bootcamp
+        </h2>
+      </Link>
     </div>
   </div>
 )

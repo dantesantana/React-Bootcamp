0a079a74ec53ccad6fb92ae8757581c5f573d697
Josh <wolfgang.101@hotmail.com>
2019-01-09 11:31:25 +1000

    Chapter 1

--- source/index.jsx
+++ source/index.jsx
@@ -1,13 +1,17 @@
+import React 	from 'react';
+import ReactDOM from 'react-dom';
 
+const myReactElement = React.createElement(
+	'button',		// this element we want to create
+	{
+		onClick: function handleClick(){
+			alert('Thanks!')
+		}
+	},				// the properties we want to add to the element
+	'Click Me!'		// a child element
+);
 
-function Main() {
-  document.body.innerHTML = `
-    <div>
-      <span>
-        Hello Hacker News
-      </span>
-    </div>
-  `
-}
-
-Main()
+ReactDOM.render(
+	myReactElement,					// what react element we want to render
+	document.getElementById('app')	// where we want to render on the page
+);

2b742a1e633f2480a3dd61b26c0fe31400854b0b
Josh <wolfgang.101@hotmail.com>
2019-01-10 16:56:08 +1000

    improving commenting

--- source/index.jsx
+++ source/index.jsx
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom';
 var count = 0;
 
 function clickHandler(){
-	count++;
-	alert('Thanks! ' + count);
+	count++; //count number of times the button is clicked
+	alert('Thanks! ' + count); //output message and click counter
 }
 
 ReactDOM.render(
 	(
+		//this could be written in a separate function, 
+		//but has been entered into the renderer here to save space
+		//(as it is only used once)
 		<button onClick={clickHandler}>Click Me!</button>
 	), 
 	document.getElementById('app')
